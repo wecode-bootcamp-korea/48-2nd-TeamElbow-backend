8e5552c7d0bec901043cd5b8593561ccd11ccd49
정동민 <jeongdongmin@jeongdongmin-ui-MacBookAir.local>
2023-08-23 03:43:15 +0900

    [modify] schema

--- db/migrations/20230822173235_modify_table_seats.sql
+++ db/migrations/20230822173235_modify_table_seats.sql
@@ -3,8 +3,10 @@ ALTER TABLE seats DROP FOREIGN KEY seats_ibfk_1;
 ALTER TABLE seats DROP COLUMN screening_id;
 ALTER TABLE seats DROP COLUMN seat_type;
 ALTER TABLE seats DROP COLUMN is_booked;
+ALTER TABLE seats ADD COLUMN theater_id int not null;
 ALTER TABLE seats ADD COLUMN seat_type_id int not null;
-ALTER TABLE seats ADD FOREIGN KEY (seat_type_id) REFERENCES seat_types(id)
+ALTER TABLE seats ADD FOREIGN KEY (seat_type_id) REFERENCES seat_types(id);
+ALTER TABLE seats ADD FOREIGN KEY (theater_id) REFERENCES theaters(id);
 
 
 -- migrate:down
