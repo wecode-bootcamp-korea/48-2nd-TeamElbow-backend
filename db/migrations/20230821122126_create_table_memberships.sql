fdac58d32faf17147fa8cc7bb60ecfa409323fb0
정동민 <jeongdongmin@jeongdongmin-ui-MacBookAir.local>
2023-08-22 18:40:11 +0900

    just commit again

--- db/migrations/20230821122126_create_table_memberships.sql
+++ db/migrations/20230821122126_create_table_memberships.sql
@@ -2,11 +2,6 @@
 CREATE TABLE `memberships` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `membership_name` varchar(50) NOT NULL,
-  `free_booking_coupon` int NOT NULL,
-  `free_snack_coupon` int NOT NULL,
-  `store_discount_coupon` int NOT NULL,
-  `free_photo_ticket_coupon` int NOT NULL,
-  `half_price_point_booking_coupon` int NOT NULL,
   `earning_point_rate` int NOT NULL
 );
 
