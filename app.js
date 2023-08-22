886c7398e70fb7b33c409dac0e407fc043095626
정동민 <jeongdongmin@jeongdongmin-ui-MacBookAir.local>
2023-08-22 12:04:46 +0900

    [delete] unnecessary comments

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-route = require('./api/routes');
-const dataSource = require('./api/models/dataSource');
-// const { globalErrorHandler } = require('./api/utils/error');
+route = require("./api/routes");
+const dataSource = require("./api/models/dataSource");
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(express.json());
-// app.use(route);
-// app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 8000;
 
 app.get("/ping", (req, res) => {
-    res.status(200).json({"message": "pong"})
-  });
-
+  res.status(200).json({ message: "pong" });
+});
 
 app.listen(PORT, async () => {
   await dataSource
     .initialize()
     .then(() => {
-      console.log('Data Source has been initialized!');
+      console.log("Data Source has been initialized!");
     })
     .catch((error) => {
-      console.error('Error during Data Source initialization', error);
+      console.error("Error during Data Source initialization", error);
     });
 
   console.log(`Listening to request on port: ${PORT}`);
