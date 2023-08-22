773513fcab56659a8e6a70613934f76757484349
정동민 <jeongdongmin@jeongdongmin-ui-MacBookAir.local>
2023-08-22 18:20:28 +0900

    [modify] typo

--- db/migrations/20230822082230_create_table_memberships_coupons.sql
+++ db/migrations/20230822082230_create_table_memberships_coupons.sql
@@ -3,8 +3,8 @@ CREATE TABLE `memberships_coupons` (
   `membership_id` int not null,
   `coupon_id` int not null,
   `the_number_of_coupon` int not null,
-  PRIMARY KEY (`member_id`, `coupon_id`),
-  FOREIGN KEY (membeship_id) REFERENCES membeships (id),
+  PRIMARY KEY (`membership_id`, `coupon_id`),
+  FOREIGN KEY (membership_id) REFERENCES memberships (id),
   FOREIGN KEY (coupon_id) REFERENCES coupons (id)
 );
 
