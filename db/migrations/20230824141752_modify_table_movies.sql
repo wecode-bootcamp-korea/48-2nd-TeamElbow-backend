ee1657bd1931d059e889f19e520868bc82ec6d53
정동민 <jeongdongmin@jeongdongmin-ui-MacBookAir.local>
2023-08-29 12:07:02 +0900

    fix migrations file

--- db/migrations/20230824141752_modify_table_movies.sql
+++ db/migrations/20230824141752_modify_table_movies.sql
@@ -2,8 +2,9 @@
 ALTER TABLE movies 
 DROP COLUMN director_and_actor,
 ADD director varchar(30) NOT NULL AFTER `poster_image_url`,
-ADD actor varchar(100) NOT NULL AFTER `director`
-MODIFY minimum_watching_age varchar(30);
+ADD actor varchar(100) NOT NULL AFTER `director`;
+ALTER TABLE movies
+MODIFY COLUMN minimum_watching_age varchar(30) NOT NULL;
 
 -- migrate:down
 
