322be7a09281abeb3482ac03a4fb46913b7b3013
김수영 <sooyoungkim@SOOYOUNGKIMmacbookAir.local>
2023-08-31 18:28:53 +0900

    [modify]booking setting

--- api/controllers/bookingController.js
+++ api/controllers/bookingController.js
@@ -2,8 +2,9 @@ const bookingService = require('../services/bookingService');
 const { catchAsync } = require('../utils/error');
 
 const getAllMoviesInformation = catchAsync(async (req, res) => {
-    const allMoviesInformation = await bookingService.getAllMoviesInformation(req);
-
+    const { sortBy } = req.query;
+    const allMoviesInformation = await bookingService.getAllMoviesInformation(sortBy);
+    
     res.status(200).json( allMoviesInformation );
 });
 
@@ -11,39 +12,15 @@ const getDate = catchAsync(async (req, res) => {
     const {movieId} = req.query;
     const date = await bookingService.getDate(movieId);
 
-    const convertedDates = date.map(item => {
-        const dateObject = new Date(item.date);
-        const formattedDate = dateObject.toISOString().split('T')[0];
-        return formattedDate;
-    });
-
-    res.status(200).json(convertedDates);
+    res.status(200).json(date);
 });
 
 const getSchedule = catchAsync(async (req, res) => {
     const {movieId, date} = req.query;
     const schedule = await bookingService.getSchedule(movieId, date);
     
-    const convertedschedule = schedule.map(item => {
-        const screeningId = item.screeningId;
-        const theaterName = item.theaterName;
-        const screeningTime = new Date(item.screeningTime);
-        const time = screeningTime.toLocaleTimeString('en-US', { hour12: false });
-        const remainingSeats= item.remainingSeats;
-        
-        
-        return {
-            screeningId,
-            theaterName,
-            time,
-            remainingSeats
-        };
-      });
-
-    res.status(200).json( convertedschedule );
+    res.status(200).json( schedule );
 });
-
-
 
 module.exports = {
     getAllMoviesInformation,
--- api/models/bookingDao.js
+++ api/models/bookingDao.js
@@ -1,8 +1,11 @@
 const {dataSource} = require('./dataSource');
 
-const getAllMoviesInformation = async (ordering) => {
-    
+
+
+const getAllMoviesInformation = async (sortBy) => {
     try {
+        const ordering = getOrdering(sortBy);
+
         const allMoviesInformation = await dataSource.query(
             `
             SELECT
@@ -16,14 +19,23 @@ const getAllMoviesInformation = async (ordering) => {
             ${ordering};
             `
         )
-        
         return allMoviesInformation;
     } catch(err) {
-        console.log(err);
         const error = new Error("dataSource Error");
         error.statusCode = 400;
 
         throw error;
+    }
+};
+
+const getOrdering = (sortBy) => {
+    switch (sortBy) {
+        case "bookingRate":
+            return `ORDER BY -bookingRatePercent`;
+        case "alphabet":
+            return `ORDER BY movieTitle`;
+        default:
+            return `ORDER BY -bookingRatePercent`;
     }
 };
 
@@ -32,7 +44,21 @@ const getDate = async (movieId) => {
         const schedule = await dataSource.query(
             `
             SELECT 
-            DISTINCT DATE(screening_time) AS date
+            DISTINCT 
+            CONCAT(DATE_FORMAT(screening_time, '%Y.%m.%d'),
+            ' (',
+            CASE DAYOFWEEK(screening_time)
+            WHEN 1 THEN '일'
+            WHEN 2 THEN '월'
+            WHEN 3 THEN '화'
+            WHEN 4 THEN '수'
+            WHEN 5 THEN '목'
+            WHEN 6 THEN '금'
+            WHEN 7 THEN '토'
+            ELSE 'Unknown'
+            END,
+            ')'
+            ) AS date
             FROM screenings
             WHERE movie_id = ?
             ORDER BY date;
@@ -41,7 +67,6 @@ const getDate = async (movieId) => {
     )
     return schedule;
     } catch(err)  {
-        console.log(err);
         const error = new Error("dataSource Error");
         error.statusCode = 400;
 
@@ -56,10 +81,15 @@ const getSchedule = async (movieId, date) => {
             SELECT 
             s.id AS screeningId,
             t.theater_name AS theaterName, 
-            s.screening_time AS screeningTime,        
+            CONCAT(
+            DATE_FORMAT(s.screening_time, '%H:%i'),
+            '~',
+            DATE_FORMAT(ADDTIME(s.screening_time, SEC_TO_TIME(m.running_time_minute * 60)), '%H:%i')
+            ) AS screeningTime,        
             (SELECT COUNT(*) FROM seats WHERE theater_id = t.id) - COUNT(bs.seat_id) AS remainingSeats 
             FROM screenings s 
-            JOIN theaters t ON s.theater_id = t.id 
+            JOIN theaters t ON s.theater_id = t.id
+            JOIN movies m ON s.movie_id = m.id
             LEFT JOIN bookings_seats bs ON s.id = (SELECT screening_id FROM bookings WHERE id = bs.booking_id) 
             WHERE s.movie_id = ?     
             AND DATE(s.screening_time) = ?
@@ -69,8 +99,8 @@ const getSchedule = async (movieId, date) => {
             [movieId, date]
     )
     return schedule;
-    } catch(err)  {
-        console.log(err);
+    } catch(err) {
+        console.error(err);
         const error = new Error("dataSource Error");
         error.statusCode = 400;
 
